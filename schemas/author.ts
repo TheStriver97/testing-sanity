756b544baa5408431601e8f6025788ff15e17ce0
TheStriver97 <nileshh97@proton.me>
2023-12-01 16:35:01 +0530

    My Project

--- schemas/author.ts
+++ schemas/author.ts
@@ -1,4 +1,6 @@
 import {defineField, defineType} from 'sanity'
+import {IconType} from 'react-icons'
+import {FaTwitter,FaInstagram,FaLinkedin} from 'react-icons/fa'
 
 export default defineType({
   name: 'author',
@@ -8,6 +10,11 @@ export default defineType({
     defineField({
       name: 'name',
       title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'Meet_Your_Instructor',
+      title: 'Instructor Name',
       type: 'string',
     }),
     defineField({
@@ -40,11 +47,90 @@ export default defineType({
         },
       ],
     }),
+    {
+      type: 'object',
+      name: 'twitterUrl',
+      title: 'Twitter',
+      fields: [
+        {
+          type: 'url',
+          name: 'url',
+          title: 'URL',
+          validation: (Rule) =>
+          Rule.regex(/^https:\/\/(www\.)?twitter\.com\/.+$/, 'i').error(
+            'Invalid Twitter URL'
+          ),
+        },
+      ],
+    },
+    {
+      type: 'object',
+      name: 'InstagramUrl',
+      title: 'Instagram',
+      fields: [
+        {
+          type: 'url',
+          name: 'url',
+          title: 'URL',
+          validation: (Rule) =>
+          Rule.regex(/^https:\/\/(www\.)?instagram\.com\/.+$/, 'i').error(
+            'Invalid Instagram URL'
+          ),
+        },
+      ],
+    },
+    {
+      type: 'object',
+      name: 'LinkedinUrl',
+      title: 'Linkedin',
+      fields: [
+        {
+          type: 'url',
+          name: 'url',
+          title: 'URL',
+          validation: (Rule) =>
+          Rule.regex(/^https:\/\/(www\.)?linkedin\.com\/.+$/, 'i').error(
+            'Invalid Linkedin URL'
+          ),
+        },
+      ],
+    },
+    
+    
   ],
   preview: {
     select: {
       title: 'name',
       media: 'image',
+      socials: 'socials[]',
+    },
+    prepare(selection) {
+      const {title, media, socials} = selection
+      // create a mapping of platform names to icons
+      const icons:{[key:string]:IconType} = {
+        Twitter: FaTwitter,
+        Instagram: FaInstagram,
+        Linkedin: FaLinkedin,
+        // add more icons as needed
+      }
+      // create a subtitle that shows the icons and names of the platforms
+      const subtitle = socials
+        .map((social:{title:string}) => {
+          // get the platform name from the title property of the social object
+          const platform = social.title
+          // get the icon component from the icons object
+          const Icon = icons[platform]
+          // return a string that contains the icon and the platform name
+          return `<${Icon} /> ${platform}`
+        })
+        // join the strings with spaces
+        .join(' ')
+      // return the preview object with the title, media and subtitle
+      return {
+        title,
+        media,
+        subtitle,
+      }
     },
   },
 })
